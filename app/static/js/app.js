5653581a4e174da7928ab0f9ab46fea309f73f52
Jordan Cizmja <78005686+Jpatt6899@users.noreply.github.com>
2021-07-27 21:45:52 -0400

    Update app.js

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -3,24 +3,18 @@
 function buildlinePlot() {
 
   const url = "/api/average_home_price";
-  d3.json(url).then(function(myData) {
-
+  d3.json(url).then(function(response) {
+   console.log(response);
    
 
-    var date = myData.Date
-    var average_home_price = myData.Average_Home_Price
-
-    var trace1 = {
-        y: average_home_price,
-        x: date,
-        type: "scatter"
-        
-    };
+    var data = response;
+    
 
     var layout = {
-        title: "Average Home Price"
+        title: "Average Home Price", 
+        type: "scatter"
     }
-    var data = [trace1]
+    
 
     Plotly.newPlot("line", data, layout);
 })
